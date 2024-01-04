b9ebc125062c7442f439fa7acf122ac4a0b5bd1f
Sancho Amaral Simões <smartlord7@gmail.com>
2024-01-04 17:51:27 +0000

    MASTER: Document data privacy metrics.

--- src/QIDLearningLib/metrics/data_privacy.py
+++ src/QIDLearningLib/metrics/data_privacy.py
@@ -1,35 +1,153 @@
+"""
+QIDLearningLib
+
+Library Description:
+QIDLearningLib is a Python library designed to provide a comprehensive set of metrics for quasi-identification recognition processes.
+The library encompasses metrics for assessing data privacy, data utility, and the performance of quasi-identification recognition algorithms.
+
+Module Description (metrics.data_privacy):
+The module in QIDLearningLib includes functions to calculate various metrics related to the data privacy regarding the assumed quasi identifiers and/or sensitive attributes.
+
+Year: 2023/2024
+Institution: University of Coimbra
+Department: Department of Informatics Engineering
+Program: Master's in Informatics Engineering - Intelligent Systems
+Author: Sancho Amaral Simões
+Student No: 2019217590
+Email: sanchoamaralsimoes@gmail.com
+Version: v0.01
+
+License:
+This open-source software is released under the terms of the GNU General Public License, version 3 (GPL-3.0).
+For more details, see https://www.gnu.org/licenses/gpl-3.0.html
+
+"""
+
 import numpy as np
+import pandas as pd
 from scipy.stats import wasserstein_distance
 from scipy.stats import entropy as scipy_entropy
-
 from structure.GroupedMetric import GroupedMetric
 
 
 def k_anonymity(df, quasi_identifiers):
+    """
+    Calculate the k-Anonymity metric for a given DataFrame and quasi-identifiers.
+
+    Synopsis:
+    k-Anonymity measures the minimum group size of quasi-identifiers in a dataset, ensuring that each group has at least
+    k identical instances.
+
+    Details:
+    The metric is computed by grouping the DataFrame based on quasi-identifiers and determining the size of each group.
+
+    Parameters:
+    - df (pd.DataFrame): The input DataFrame.
+    - quasi_identifiers (list): List of column names representing quasi-identifiers.
+
+    Return:
+    GroupedMetric: k-Anonymity metric for each group.
+
+    Example:
+    >>> k_anonymity_metric = k_anonymity(df, ['Age', 'Gender'])
+    >>> print(repr(k_anonymity_metric))
+    """
+
+    # Group the DataFrame based on quasi_identifiers
     grouped = df.groupby(quasi_identifiers)
+
+    # Extract group labels from the grouped DataFrame
     group_labels = [group_name for group_name, _ in grouped]
 
+    # Create a GroupedMetric object with the calculated k-Anonymity values, group labels, and a name
     return GroupedMetric(np.array(grouped.size()), group_labels, name='k-Anonymity')
 
-def l_diversity(df, quasi_identifiers, sensitive_attribute):
+
+def l_diversity(df: pd.DataFrame, quasi_identifiers: list, sensitive_attributes: list) -> GroupedMetric:
+    """
+    Calculate the l-Diversity metric for a given DataFrame, quasi-identifiers, and sensitive attribute.
+
+    Synopsis:
+    l-Diversity measures the minimum number of unique values in the sensitive attribute within each group defined by 
+    quasi-identifiers.
+
+    Details:
+    The metric is computed by grouping the DataFrame based on quasi-identifiers and counting the unique values in the
+    sensitive attribute for each group.
+
+    Parameters:
+    - df (pd.DataFrame): The input DataFrame.
+    - quasi_identifiers (list): List of column names representing quasi-identifiers.
+    - sensitive_attributes (str): The sensitive attribute for which l-Diversity is calculated.
+
+    Return:
+    GroupedMetric: l-Diversity metric for each group.
+
+    Example:
+    >>> l_diversity_metric = l_diversity(df, ['Age', 'Gender'], ['Disease'])
+    >>> print(repr(l_diversity_metric))
+    """
+
+    # Group the DataFrame based on quasi_identifiers
     grouped = df.groupby(quasi_identifiers)
+
+    # Initialize an empty list to store l-Diversity values for each group
     unique_values = []
 
+    group_labels = []
+
+    # Iterate over each group in the grouped DataFrame
     for group_name, group_df in grouped:
-        unique_values.append(group_df[sensitive_attribute].nunique())
+        # Count the unique values in each sensitive attribute for the current group
+        unique_values.extend(group_df[sensitive_attributes].nunique().values)
+        group_labels.append(group_name)
 
-    group_labels = [group_name for group_name, _ in grouped]
-
+    # Create a GroupedMetric object with the calculated l-Diversity values, group labels, and a name
     return GroupedMetric(np.array(unique_values), group_labels, name='l-Diversity')
 
-def closeness_centrality(df, quasi_identifiers, sensitive_attribute):
-    overall_distribution = df[sensitive_attribute].value_counts(normalize=True)
+
+def closeness_centrality(df: pd.DataFrame, quasi_identifiers: list, sensitive_attributes: list):
+    """
+    Calculate the Closeness Centrality metric for a given DataFrame, quasi-identifiers, and sensitive attribute.
+
+    Synopsis:
+    Closeness Centrality measures the Wasserstein distance between the overall distribution of the sensitive attribute
+    and the distribution within each group defined by quasi-identifiers.
+
+    Details:
+    The metric is computed by grouping the DataFrame based on quasi-identifiers and calculating the Wasserstein distance
+    between the overall distribution and the distribution within each group.
+
+    Parameters:
+    - df (pd.DataFrame): The input DataFrame.
+    - quasi_identifiers (list): List of column names representing quasi-identifiers.
+    - sensitive_attributes (str): The sensitive attribute for which Closeness Centrality is calculated.
+
+    Return:
+    GroupedMetric: Closeness Centrality metric for each group.
+
+    Example:
+    >>> closeness_centrality_metric = closeness_centrality(df, ['Age', 'Gender'], ['Income'])
+    >>> print(repr(closeness_centrality_metric))
+    """
+
+    # Calculate the overall distribution of the sensitive attribute
+    overall_distribution = df[sensitive_attributes].value_counts(normalize=True)
+
+    # Group the DataFrame based on quasi_identifiers
     grouped = df.groupby(quasi_identifiers)
 
+    # Initialize an empty list to store Closeness Centrality values for each group
     closeness_values = []
-    for group_name, group_df in grouped:
-        group_distribution = group_df[sensitive_attribute].value_counts(normalize=True)
 
+    group_labels = []
+
+    # Iterate over each group in the grouped DataFrame
+    for group_name, group_df in grouped:
+        # Calculate the distribution of the sensitive attribute within the current group
+        group_distribution = group_df[sensitive_attributes].value_counts(normalize=True)
+
+        # Check if the values in group_distribution are numeric
         if all(isinstance(val, (int, float)) for val in group_distribution.index):
             closeness = wasserstein_distance(overall_distribution.index.astype(float),
                                              group_distribution.index.astype(float), overall_distribution.values,
@@ -43,64 +161,135 @@ def closeness_centrality(df, quasi_identifiers, sensitive_attribute):
                                              overall_distribution.values, group_distribution.values)
 
         closeness_values.append(closeness)
+        group_labels.append(group_name)
 
-    group_labels = [group_name for group_name, _ in grouped]
-
+    # Create a GroupedMetric object with the calculated Closeness Centrality values, group labels, and a name
     return GroupedMetric(np.array(closeness_values), group_labels, name='Closeness Centrality')
 
-def delta_presence(df, quasi_identifier, value):
-    overall_presence = df[quasi_identifier].value_counts(normalize=True).get(value, 0)
-    grouped = df.groupby(quasi_identifier)
 
-    delta_presence_values = []
-    for group_name, group_df in grouped:
-        group_presence = group_df[quasi_identifier].value_counts(normalize=True).get(value, 0)
-        delta_presence = abs(overall_presence - group_presence)
-        delta_presence_values.append(delta_presence)
+def delta_presence(df: pd.DataFrame, quasi_identifiers: list, values: list) -> GroupedMetric:
+    """
+    Calculate the Delta Presence metric for a given DataFrame, quasi-identifier, and value.
 
-    group_labels = [group_name for group_name, _ in grouped]
+    Synopsis:
+    Delta Presence measures the absolute difference in the presence of a specific value in the quasi-identifier across
+    the entire dataset and within each group.
 
-    return GroupedMetric(np.array(delta_presence_values), group_labels, name='Delta Presence')
+    Details:
+    The metric is computed by comparing the presence of a specific value in the quasi-identifier across the entire dataset
+    and within each group defined by the quasi-identifier.
 
-def t_closeness(df, quasi_identifiers, sensitive_attribute):
-    overall_distribution = df[sensitive_attribute].value_counts(normalize=True)
+    Parameters:
+    - df (pd.DataFrame): The input DataFrame.
+    - quasi_identifier (str): The quasi-identifier for which Delta Presence is calculated.
+    - value: The value for which Delta Presence is calculated.
+
+    Return:
+    GroupedMetric: Delta Presence metric for each group.
+
+    Example:
+    >>> delta_presence_metric = delta_presence(df, 'Age', 25)
+    >>> print(repr(delta_presence_metric))
+    """
+
+    # Calculate the overall presence of the values in the quasi_identifiers across the entire dataset
+    overall_presence = df[quasi_identifiers].value_counts(normalize=True).get(tuple(values), 0)
+
+    # Group the DataFrame based on quasi_identifier
     grouped = df.groupby(quasi_identifiers)
 
-    t_closeness_values = []
+    # Initialize an empty list to store Delta Presence values for each group
+    delta_presence_values = []
+
+    group_labels = []
+
+    # Iterate over each group in the grouped DataFrame
     for group_name, group_df in grouped:
-        group_distribution = group_df[sensitive_attribute].value_counts(normalize=True)
+        # Calculate the presence of the value in the quasi_identifier within the current group
+        group_presence = group_df[quasi_identifiers].value_counts(normalize=True).get(tuple(values), 0)
 
-        # Create a union of unique values from both distributions
-        all_values = set(overall_distribution.index).union(group_distribution.index)
+        # Calculate Delta Presence for the current group
+        delta_presence = abs(overall_presence - group_presence)
+        delta_presence_values.append(delta_presence)
+        group_labels.append(group_name)
 
-        # Fill missing values with zero probability
-        overall_distribution = overall_distribution.reindex(all_values, fill_value=0)
-        group_distribution = group_distribution.reindex(all_values, fill_value=0)
+    # Create a GroupedMetric object with the calculated Delta Presence values, group labels, and a name
+    return GroupedMetric(np.array(delta_presence_values), group_labels, name='Delta Presence')
+
+
+def t_closeness(df: pd.DataFrame, quasi_identifiers: list, sensitive_attributes : list):
+    """
+    Calculate the t-Closeness metric for a given DataFrame, quasi-identifiers, and sensitive attribute.
+
+    Synopsis:
+    t-Closeness measures the Kullback-Leibler (KL) divergence between the distribution of the sensitive attribute within
+    each group defined by quasi-identifiers and the overall distribution.
+
+    Details:
+    The metric is computed by grouping the DataFrame based on quasi-identifiers and calculating the KL divergence for
+    each group.
+
+    Parameters:
+    - df (pd.DataFrame): The input DataFrame.
+    - quasi_identifiers (list): List of column names representing quasi-identifiers.
+    - sensitive_attributes (str): The sensitive attribute for which t-Closeness is calculated.
+
+    Return:
+    GroupedMetric: t-Closeness metric for each group.
+
+    Example:
+    >>> t_closeness_metric = t_closeness(df, ['Age', 'Gender'], ['Income'])
+    >>> print(repr(t_closeness_metric))
+    """
+
+    # Calculate the overall distribution of the sensitive attribute
+    overall_distribution = df[sensitive_attributes].apply(lambda x: tuple(x), axis=1).value_counts(normalize=True)
+
+    # Group the DataFrame based on quasi_identifiers
+    grouped = df.groupby(quasi_identifiers)
+
+    # Initialize an empty list to store t-Closeness values for each group
+    t_closeness_values = []
+
+    group_labels = []
+
+    # Iterate over each group in the grouped DataFrame
+    for group_name, group_df in grouped:
+        # Calculate the distribution of the sensitive attribute within the current group
+        group_distribution = group_df[sensitive_attributes].apply(lambda x: tuple(x), axis=1).value_counts(
+            normalize=True)
+
+        # Ensure that both distributions have the same index
+        common_index = overall_distribution.index.union(group_distribution.index)
+        overall_distribution = overall_distribution.reindex(common_index, fill_value=0)
+        group_distribution = group_distribution.reindex(common_index, fill_value=0)
 
         # Calculate KL divergence using the entropy function
         kl_divergence = scipy_entropy(group_distribution, qk=overall_distribution)
 
         t_closeness_values.append(kl_divergence)
 
-    group_labels = [group_name for group_name, _ in grouped]
+        group_labels.append(group_name)
 
     return GroupedMetric(np.array(t_closeness_values), group_labels, name='t-Closeness')
 
-def generalization_ratio(df, quasi_identifiers, sensitive_attribute):
-    overall_distribution = df[sensitive_attribute].value_counts(normalize=True)
+
+def generalization_ratio(df: pd.DataFrame, quasi_identifiers: list, sensitive_attributes: list) -> GroupedMetric:
+    overall_distribution = df[sensitive_attributes].value_counts(normalize=True)
     grouped = df.groupby(quasi_identifiers)
 
     generalization_ratio_values = []
     for group_name, group_df in grouped:
-        group_distribution = group_df[sensitive_attribute].value_counts(normalize=True)
+        group_distribution = group_df[sensitive_attributes].value_counts(normalize=True)
         generalization_ratio_values.append(np.sum(np.abs(overall_distribution - group_distribution)))
 
     group_labels = [group_name for group_name, _ in grouped]
 
     return GroupedMetric(np.array(generalization_ratio_values), group_labels, name='Generalization Ratio')
 
-def reciprocal_rank(df, quasi_identifiers, sensitive_attribute):
-    ranks = df.groupby(quasi_identifiers)[sensitive_attribute].rank(ascending=False)
+
+def reciprocal_rank(df: pd.DataFrame, quasi_identifiers: list, sensitive_attributes: list) -> GroupedMetric:
+    ranks = df.groupby(quasi_identifiers)[sensitive_attributes].rank(ascending=False)
     reciprocal_ranks = 1 / ranks
     mrr_values = reciprocal_ranks.values
 

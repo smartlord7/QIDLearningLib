a3f1a407ebbc6d48d7a81ea570fae3cf459c288e
Sancho Amaral Simões <smartlord7@gmail.com>
2024-10-30 10:17:24 +0000

    MASTER: Rollback data privacy test script.

--- src/QIDLearningLib/QIDLearningLib/test/data_privacy.py
+++ src/QIDLearningLib/QIDLearningLib/test/data_privacy.py
@@ -23,27 +23,40 @@ For more details, see https://www.gnu.org/licenses/gpl-3.0.html
 
 """
 
-from metrics.data_privacy import *
-from util.data import generate_synthetic_dataset
-from util.time import run_tests
+from QIDLearningLib.metrics.data_privacy import *
+from QIDLearningLib.util.data import generate_synthetic_dataset
 
 
+def analyze_privacy_metrics(df, quasi_identifiers, sensitive_attribute):
+    print("k-Anonymity:")
+    k_anonymity_metric = k_anonymity(df, quasi_identifiers)
+    print(repr(k_anonymity_metric))
+    k_anonymity_metric.plot_all()
 
+    print("\nl-Diversity:")
+    l_diversity_metric = l_diversity(df, quasi_identifiers, [sensitive_attribute, ])
+    print(repr(l_diversity_metric))
+    l_diversity_metric.plot_all()
 
-def analyze_privacy_metrics(df, quasi_identifiers, sensitive_attributes):
-    test_cases = [
-        (k_anonymity_old, k_anonymity, quasi_identifiers),
-        (closeness_centrality_old, closeness_centrality, quasi_identifiers, sensitive_attributes),
-        (delta_presence_old, delta_presence, ['Age'], [22, ]),
-        (t_closeness_old, t_closeness, quasi_identifiers, sensitive_attributes),
-        (generalization_ratio_old, generalization_ratio, quasi_identifiers, sensitive_attributes),
-        (reciprocal_rank, reciprocal_rank, quasi_identifiers, sensitive_attributes),
+    print("\nCloseness Centrality:")
+    closeness_centrality_metric = closeness_centrality(df, quasi_identifiers, [sensitive_attribute, ])
+    print(repr(closeness_centrality_metric))
+    closeness_centrality_metric.plot_all()
 
-    ]
+    print("\nt-Closeness:")
+    t_closeness_metric = t_closeness(df, quasi_identifiers, [sensitive_attribute, ])
+    print(repr(t_closeness_metric))
+    t_closeness_metric.plot_all()
 
-    # Run the tests
-    run_tests(test_cases, df, 3)
+    print("\nDelta Presence:")
+    delta_presence_metric = delta_presence(df, ['Age', ], [22, ])
+    print(repr(delta_presence_metric))
+    delta_presence_metric.plot_all()
 
+    print("\nGeneralization Ratio:")
+    generalization_ratio_metric = generalization_ratio(df, quasi_identifiers, [sensitive_attribute, ])
+    print(repr(generalization_ratio_metric))
+    generalization_ratio_metric.plot_all()
 
 
 def main():
@@ -52,7 +65,7 @@ def main():
 
     # Example usage
     quasi_identifiers = ['Age', 'Gender', 'Country', 'Education']
-    sensitive_attribute = ['Disease']
+    sensitive_attribute = 'Disease'
 
     # Analyze privacy metrics
     analyze_privacy_metrics(df, quasi_identifiers, sensitive_attribute)
